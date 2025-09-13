a964c7adf54045c0df5fcffae106b78f180ac9c4
Daniel Lang <me@daniellang.net>
2025-09-13 15:44:02 -0700

    Using dotenv

--- run_worker.py
+++ run_worker.py
@@ -1,18 +1,26 @@
 import asyncio
+import os
+from dotenv import load_dotenv
 
-from temporalio import activity, workflow
 from temporalio.client import Client
 from temporalio.worker import Worker
 
 from activities import say_hello
 from workflows import SayHello
 
+# Load environment variables from a .env file (if present)
+load_dotenv()
+
 async def main():
+    temporal_address = os.getenv("TEMPORAL_ADDRESS", "localhost:7233")
+    temporal_namespace = os.getenv("TEMPORAL_NAMESPACE", "default")
+    temporal_task_queue = os.getenv("TEMPORAL_TASK_QUEUE", "hello-task-queue")
+
     print("Connecting to Temporal Service")
-    client = await Client.connect("localhost:7233", namespace="default")
+    client = await Client.connect(temporal_address, namespace=temporal_namespace)
 
     print("Run worker")
-    worker = Worker(client, task_queue="hello-task-queue", workflows=[SayHello], activities=[say_hello])
+    worker = Worker(client, task_queue=temporal_task_queue, workflows=[SayHello], activities=[say_hello])
     await worker.run()
 
 if __name__ == "__main__":
--- run_workflow.py
+++ run_workflow.py
@@ -1,12 +1,23 @@
 import asyncio
+import os
+from dotenv import load_dotenv
 
 from workflows import SayHello
 from temporalio.client import Client
 
-async def main():
-    client = await Client.connect("localhost:7233")
+# Load environment variables from a .env file (if present)
+load_dotenv()
 
-    result = await client.execute_workflow(SayHello.run, "Daniel", id="hello-workflow", task_queue="hello-task-queue")
+async def main():
+    temporal_address = os.getenv("TEMPORAL_ADDRESS", "localhost:7233")
+    temporal_namespace = os.getenv("TEMPORAL_NAMESPACE", "default")
+    temporal_task_queue = os.getenv("TEMPORAL_TASK_QUEUE", "hello-task-queue")
+
+    print("Connecting to Temporal Service")
+    client = await Client.connect(temporal_address, namespace=temporal_namespace)
+
+    print("Executing workflow")
+    result = await client.execute_workflow(SayHello.run, "Daniel", id="hello-workflow", task_queue=temporal_task_queue)
 
     print(f"Result: {result}")
 
